3880068a9db3a1151f2b8bd92fb85488acced2c6
Mohamed Ahmed Mohamed Dawood <39738173+mohameddawood53@users.noreply.github.com>
2024-01-26 23:40:23 +0200

    Initial commit

---
+++ content/js/fatoria.js
@@ -0,0 +1,7 @@
+let navebutton =document.querySelector(".bar-button");
+let div = document.querySelector(".nave-pages");
+
+navebutton.addEventListener('click',()=>{
+
+    div.classList.toggle("hide-show")
+})
